0c0baf64c0b7ae52bbf4e6fcb4529dcfbeb9e1fa
Ethan Capehart <ecape0207@gmail.com>
2026-09-24 20:48:53 -0600

    Changed tests for task 2 and 6 to use parametrize per rubric

--- homework1/tests/test_task2.py
+++ homework1/tests/test_task2.py
@@ -1,14 +1,27 @@
 from src.task2 import add, same_length, combine_string 
+import pytest
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1.5, 1.5) == 3
-    assert add(1, 2.5) == 3.5
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    (1.5, 1.5, 3),
+    (1, 2.5, 3.5)
+])
 
-def test_same_length():
-    assert same_length("Potato", "Tomato") == True
-    assert same_length("Success", "Fail") == False
+def test_add(a, b, expected):
+    assert add(a, b) == expected
 
-def test_combine_string():
-    assert combine_string("What", "ToDO") == "WhatToDO"
-    assert combine_string("What", " am I doing") == "What am I doing"
+@pytest.mark.parametrize("a, b, expected", [
+    ("Potato", "Tomato", True),
+    ("Success", "Fail", False)
+])
+
+def test_same_length(a, b, expected):
+    assert same_length(a, b) == expected
+
+@pytest.mark.parametrize("a, b, expected", [
+    ("What", "ToDO", "WhatToDO"),
+    ("What", " am I doing", "What am I doing")
+])
+
+def test_combine_string(a, b, expected):
+    assert combine_string(a, b) == expected
--- homework1/tests/test_task6.py
+++ homework1/tests/test_task6.py
@@ -1,5 +1,7 @@
 from src.task6 import open_file, count_words
 from pathlib import Path
+import pytest
+
 TEST_FILE = Path(__file__).parent.parent / "task6_read_me.txt"
 
 def test_open_file():
@@ -7,6 +9,10 @@ def test_open_file():
 
     assert open_file("this/path/doesnt/exist") == None
 
-def test_count_words():
-    assert count_words("This is four words.") == 4
-    assert count_words(open_file(TEST_FILE)) == 104
+@pytest.mark.parametrize("text, expected", [
+    ("This is four words.", 4),
+    (open_file(TEST_FILE), 104)
+])
+
+def test_count_words(text, expected):
+    assert count_words(text) == expected
